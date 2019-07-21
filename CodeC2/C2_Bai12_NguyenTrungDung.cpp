e5f82f8d4fdd8fc82d4a2366d98fef7ccbe91319
trungdung0612 <hocladieutatyeu@gmail.com>
2019-07-21 21:36:58 +0700

    code c2

--- CodeC2/C2_Bai12_NguyenTrungDung.cpp
+++ CodeC2/C2_Bai12_NguyenTrungDung.cpp
@@ -1,193 +1,367 @@
-#include < iostream >
+#include <iostream>
+
 #include <stdio.h>
+
 using namespace std;
 
+
+
 struct Node
+
 {
+
 	int info;
-	Node *next,*previous;
+
+	Node *next,
+
+		*previous;
+
 };
 
-Node *first , *last ;
+Node *first, *last;
 
 void init()
+
 {
-	first=NULL;
-	last=NULL;
+
+	first = NULL;
+
+	last = NULL;
+
 }
 
 Node *ds;
-void process()
+
+void xuat()
+
 {
+
 	Node *p = new Node;
-	p=ds;
-	while(p!=NULL)
+
+	p = ds;
+
+	while (p != NULL)
+
 	{
-		cout<<p->info<< "   " ;
-		p=p->next;
+
+		cout << p->info << "  ";
+
+		p = p->next;                   //HAM NAY SAI, XEM LAI.
+
 	}
-	cout<<endl;
+
+	cout << endl;
+
 }
 
-void insertfirst(int x)
+void themDau(int x)
+
 {
+
 	Node *p;
-	p=new Node;
-	p->info=x;
-	p->next=first;
-	p->previous =NULL;
-	if(first!=NULL )
-		first->previous;
-	else
-		last=p;
-	first=p;
+
+	p = new Node;
+
+	p->info = x;
+
+	p->next = first;
+
+	p->previous = NULL;
+
+	if (first != NULL)
+
+		first->previous = p;
+
+	else //ds rong. truoc khi them p
+
+		last = p; // sau khi them p, ds co 1 phan tu
+
+	first = p;
+
 }
 
-void insertlast(int x)
+void themCuoi(int x)
+
 {
-	Node*p;
-	p=new Node;
-	p->info=x;
-	p->next=NULL;
-	p->previous=last;
-	if(last !=NULL )
-		last->next=p;
-	else
-		first=p;
-	last=p;
+
+	Node *p;
+
+	p = new Node;
+
+	p->info = x;
+
+	p->next = NULL;
+
+	p->previous = last;
+
+	if (last != NULL)
+
+		last->next = p;
+
+	else //ds rong truoc khi them p
+
+		first = p; // sau khi them p, ds co 1 phan tu
+
+	last = p;
+
 }
 
-int deletefirst()
+int xoaDau()
+
 {
-	if ( first!=NULL )
+
+	if (first != NULL)
+
 	{
-		Node*p=first;
-		first=first->next;
+
+		Node *p = first;
+
+		first = first->next;
+
 		delete p;
-		if(first!=NULL)
+
+		if (first != NULL)
+
 			first->previous;
+
 		else
-			last=NULL;
+
+			last = NULL;
+
 		return 1;
+
 	}
+
 	return 0;
+
 }
 
-int deletelast()
+int xoaCuoi()
+
 {
-	if(last!=NULL)
+
+	if (last != NULL)
+
 	{
-		Node *p =last;
-		last=last->previous;
-		if(last !=NULL)
-		last->next=NULL;
+
+		Node *p = last;
+
+		last = last->previous;
+
+		if (last != NULL)
+
+			last->next = NULL;
+
 		else
-			first=NULL;
+
+			first = NULL;
+
 		delete p;
+
 		return 1;
+
 	}
+
 	return 0;
-}
-Node *search(int y )
-{
-	Node*p;
-	p=first;
-	while(p!=NULL && p->info!=y)
-		p=p->next;
-	return p;
+
 }
 
-int Delete(int y)
+Node *Search(int y)
+
 {
-	Node*p=search(y);
-	if(p!=NULL)
-	{
-		if(p==first)
-		{
-			if(deletefirst())
-				return 1;
-			else
-				return 0;
-		}
-		else
-			if(p==last)
-			{
-				if(deletelast())
-					return 1;
-				else
-					return 0;
-			}
-		else
-		{
-			Node *q;
-			q=p->previous;
-			q->next=p->next;
-			(p->next)->previous;
-			delete p;
-			return 1;
-			}
-	}
-	return 0;
+
+	Node *p;
+
+	p = first;
+
+	while ((p != NULL) && (p->info != y))
+
+		p = p->next;
+
+	return p;
+
 }
+
+int xoa(int y)
+
+{
+
+	Node *p = Search(y);
+
+	if (p != NULL)
+
+	{
+
+		if (p == first)
+
+		{
+
+			if (xoaDau())
+
+				return 1;
+
+			else return 0;
+
+		}
+
+		else if (p == last)
+
+		{
+
+			if (xoaCuoi())
+
+				return 1;
+
+			else return 0;
+
+		}
+
+		else
+
+		{
+
+			Node *q;
+
+			q = p->previous;
+
+			q->next = p->next;
+
+			(p->next)->previous = q;
+
+			delete p;
+
+			return 1;
+
+		}
+
+	}
+
+	return 0;
+
+}
+
+
 
 int main()
+
 {
-	int choice,x;
-	do{
-		cout<<"1.khoi tao danh sach\n2.xuat danh sach\n3.them vao dau danh sach\n4.them phan tu vao cuoi danh sach\n5.xoa phan tu dau\n6.xoa phan tu cuoi\n.7.tim va xoa phan tu\n8.thoat\n " << endl;
-		cin >> choice;
-		switch(choice)
+
+	int chon;
+
+	char tt;
+
+	do {
+
+		system("cls");
+
+		cout << "1.Khoi tao danh sach rong\n2.Xuat danh sach\n3.Them 1 phan tu vao dau danh sach\n4.Them 1 phan tu cuoi danh sach\n5.Xoa phan tu dau\n6.Xoa phan tu cuoi\n7.Tim va xoa phan tu\nBan chon: ";
+
+		cin >> chon;
+
+		switch (chon)
+
 		{
+
 		case 1:
+
 			init();
-			cout<<"khoi tao danh sach rong thanh cong ";
+
+			cout << "Khoi tao ds rong thanh cong!";
+
 			break;
+
 		case 2:
-			cout<<"danh sach hien tai" << endl;
-			process();
+
+			cout << "Danh sach hien tai la: ";
+
+			xuat();
+
 			break;
+
 		case 3:
+
 			int x;
-			cout<<"nhap gia tri x can tim " << endl;
-			cin>> x;
-			insertfirst(x);
-			cout<<"them thanh cong" << endl;
+
+			cout << "Nhap gia tri can them dau:";
+
+			cin >> x;
+
+			themDau(x);
+
+			cout << "Them thanh cong ";
+
 			break;
+
 		case 4:
-			cout<<"nhap gia tri x can tim " << endl;
-			cin>> x;
-			insertlast(x);
-			cout<<"them thanh cong" << endl;
+
+			cout << "Nhap gia tri can them cuoi:";
+
+			cin >> x;
+
+			themCuoi(x);
+
+			cout << "Them thanh cong ";
+
 			break;
+
 		case 5:
-			deletefirst();
-			cout<<"danh sach sau khi xoa"<< endl;
-			process();
+
+			xoaDau();
+
+			cout << "Danh sach sau khi xoa dau: ";
+
+			xuat();
+
 			break;
+
 		case 6:
-			deletelast();
-			cout<<"ds sau khi xoa" << endl;
-			process();
+
+			xoaCuoi();
+
+			cout << "Danh sach sau khi xoa cuoi: ";
+
+			xuat();
+
 			break;
+
 		case 7:
+
 			int y;
-			cout<<"nhap ptu can xoa" << endl;
+
+			cout << "Nhap phan tu can xoa: ";
+
 			cin >> y;
-			if(search(y)==NULL)
-				cout<<"khong co phan tu can tim " << endl;
+
+			if (Search(y) == NULL)
+
+				cout << "Khong co phan tu can tim.";
+
 			else
+
 			{
-				Delete(y);
-				cout<<"ds sau khi xoa" << endl;
-				process();
+
+				xoa(y);
+
+				cout << "Xoa thanh cong. Danh sach hien tai la: ";
+
+				xuat();
+
 			}
+
 			break;
-		case 8:
-			cout<<"goodbye----------" << endl;
-			break;
-		default:
-			break;
+
+
+
 		}
-	}while(choice !=8);
-	return 0;
+
+		cout << "Ban co muon tiep tuc? (Y/N)";
+
+		cin >> tt;
+
+	} while (tt == 'y' || tt == 'Y');
+
+	system("pause");
+
+	return  0;
+
 }
